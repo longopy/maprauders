38e9cc9adf530fb4ac8423d37539902ae2985651
longo <javier_longobardo@hotmail.com>
2023-03-24 16:26:15 +0100

    Modified lang structure

--- js/lang.js
+++ js/lang.js
@@ -1,15 +1,41 @@
 import $ from 'jquery';
 
+function detectLanguage() {
+    const lang = localStorage.getItem('lang', 'en')
+}
+
+function loadLangButton() {
+    const langBtn = document.getElementById('lang-btn');
+    const lang = getTheOtherOption();
+    langBtn.src=`../data/lang/${lang}.svg`;
+}
+
+function loadTitle(){
+    const lang = localStorage.getItem('lang', 'en')
+    const titles ={
+        en: 'Marauders Interactive Maps',
+        es: 'Mapas Interactivos Marauders'
+    }
+    document.title = titles[lang]; 
+}
+
+function getTheOtherOption(){
+    const lang = localStorage.getItem('lang', 'en')
+    if (lang === 'en') {
+        return 'es';
+    }else{
+        return 'en';
+    }
+}
 $(function (){
+    detectLanguage();
+    loadTitle();
+    loadLangButton();
     $('#change-language-btn').on('click', function (e){
         e.preventDefault();
-        let location = window.location.href;
-        const lang = localStorage.getItem('lang');
-        if (lang === 'es') {
-            location = location.replace(/\/es\//, '/en/');
-        }else{
-            location = location.replace(/\/en\//, '/es/');
-        }
-        window.location.href = location;
+        console.log(e)
+        const lang = getTheOtherOption();
+        localStorage.setItem('lang', lang);
+        window.location.reload();
     });
 });
